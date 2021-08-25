a176c39d65515347c43dc6e6386dd1339ece8524
testacc-art <74711697+testacc-art@users.noreply.github.com>
2021-08-25 18:34:29 +0300

    Update DownloadComponentsWithRetry.ps1

--- DownloadComponentsWithRetry.ps1
+++ DownloadComponentsWithRetry.ps1
@@ -53,11 +53,6 @@ $bootstrapperUrl = "https://aka.ms/vs/${subVersion}/${channel}/vs_${releaseInPat
 $BootstrapperName = [IO.Path]::GetFileName($BootstrapperUrl)
 $bootstrapperFilePath = Start-DownloadWithRetry -Url $BootstrapperUrl -Name $BootstrapperName
 $WorkLoads = @(
-    "--add Microsoft.VisualStudio.Component.VC.v141.MFC.ARM.Spectre",
-    "--add Microsoft.VisualStudio.Component.VC.v141.MFC.ARM64.Spectre",
-    "--add Microsoft.VisualStudio.Component.VC.ATLMFC",
-    "--add Microsoft.VisualStudio.Component.VC.ATLMFC.Spectre",
-    "--add Microsoft.VisualStudio.Component.VC.MFC.ARM",
     "--add Microsoft.VisualStudio.Component.VC.MFC.ARM.Spectre",
     "--add Microsoft.VisualStudio.Component.VC.MFC.ARM64",
     "--add Microsoft.VisualStudio.Component.VC.MFC.ARM64.Spectre",
